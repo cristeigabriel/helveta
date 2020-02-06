a1df9f13dd3d42b5cb2a1bf1ec52a60d577ec498
Gabe! <51248378+cristeigabriel@users.noreply.github.com>
2020-02-06 19:47:31 +0200

    ❤️ first push / math

---
+++ helveta/math/initializer.hh
@@ -0,0 +1,16 @@
+/*
+
+        *project name: helveta
+        *purpose: multi-purpose shared library for windows, made to be submodule
+   for projects *written by: Cristei Gabriel-Marian *licensing: MIT License
+
+*/
+
+// STL
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+// library files
+#include "vector.hh"
