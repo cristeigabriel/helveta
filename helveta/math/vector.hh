0fed46967dfa830d19a0b52fc673038fe2b77aaa
Gabe! <51248378+cristeigabriel@users.noreply.github.com>
2020-02-06 19:48:21 +0200

    ❤️ quick-fix #1

--- helveta/math/vector.hh
+++ helveta/math/vector.hh
@@ -1,8 +1,9 @@
 /*
 
         *project name: helveta
-        *purpose: multi-purpose shared library for windows, made to be submodule
-   for projects *written by: Cristei Gabriel-Marian *licensing: MIT License
+        *purpose: multi-purpose shared library for windows, made to be submodule for projects
+        *written by: Cristei Gabriel-Marian
+        *licensing: MIT License
 
 */
 
