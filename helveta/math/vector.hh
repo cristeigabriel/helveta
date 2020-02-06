328a143f844ebc4d4a587905b07681f811b036d3
Gabe! <51248378+cristeigabriel@users.noreply.github.com>
2020-02-06 19:50:15 +0200

    ❤️ description #2

--- helveta/math/vector.hh
+++ helveta/math/vector.hh
@@ -4,7 +4,8 @@
         *purpose: multi-purpose shared library for windows, made to be submodule for projects
         *written by: Cristei Gabriel-Marian
         *licensing: MIT License
-
+       
+        *file description: the vector class used in helveta.
 */
 
 namespace util {
