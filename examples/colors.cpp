28ef2a5be6a79010b509d12b3e55ba8479cb0071
Gabe! <51248378+cristeigabriel@users.noreply.github.com>
2020-02-09 07:03:41 +0200

    📓 fixed, accidentaly used other code

--- examples/colors.cpp
+++ examples/colors.cpp
@@ -5,14 +5,28 @@ int main() {
   helveta::color::color_t<int> _color  = {255, 0, 0};        // red, alpha 255
   helveta::color::color_t<int> __color = {125, 0, 0}; // dark red, alpha 255
 
-  // we are going to prind color's brightness then _color's brightness, then
+  // we are going to get the brightness of color, _color and __color
   // __color's brightness
   std::cout << "color : " << color.brightness()
             << " _color : " << _color.brightness()
             << " __color : " << __color.brightness() << std::endl;
 
-  // we are going to get the hues of color, _color and __color
+  // we are going to get the hue of color, _color and __color
   std::cout << "color : " << color.hue() << " _color : " << _color.hue()
-            << " __color : " << __color.hue << std::endl;
+            << " __color : " << __color.hue() << std::endl;
+
+  // we are going to get the saturation of color, _color and __color
+  std::cout << "color : " << color.saturation()
+            << " _color : " << _color.saturation()
+            << " __color : " << __color.hue() << std::endl;
+
+  // we are going to print the alpha of color, _color and __color
+  std::cout << "color : " << color.a << " _color : " << _color.a
+            << " __color : " << __color.a << std::endl;
+
+  // we are going to print the alpha of color, _color and __color, but by index
+  std::cout << "color : " << color[3] << " _color : " << _color[3]
+            << " __color : " << __color[3] << std::endl;
+
   return 0;
 }
