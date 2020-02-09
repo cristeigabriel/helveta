e69cfe201f040ba25be097e4e1311fa0a81776e9
Gabe! <51248378+cristeigabriel@users.noreply.github.com>
2020-02-09 14:49:20 +0200

    ❤️ credits #4

--- helveta/misc/hasher.hh
+++ helveta/misc/hasher.hh
@@ -2,6 +2,7 @@
 *project name: helveta
 *purpose: multi-purpose shared library for windows, made to be submodule for projects 
 *written by: Cristei Gabriel-Marian
+*file written by: @T0b1-iOS + @cristeigabriel
 *licensing: MIT License
 
 *file description: fnv-1a algorithm hasher, at runtime
