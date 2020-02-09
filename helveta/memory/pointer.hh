9b673793f8e60503eccc04de627b697191ddd710
T0b1 <tasumifox@gmail.com>
2020-02-09 03:05:11 +0100

    fixing line endings

--- helveta/memory/pointer.hh
+++ helveta/memory/pointer.hh
@@ -1,7 +1,9 @@
 /*
 *project name: helveta
 *purpose: multi-purpose shared library for windows, made to be submodule for
-projects *written by: Cristei Gabriel *file written by: T0b1-iOS *licensing: MIT
+projects
+*written by: Cristei Gabriel
+*file written by: T0b1-iOS *licensing: MIT
 License
 
 *file description: external pointer wrapper
