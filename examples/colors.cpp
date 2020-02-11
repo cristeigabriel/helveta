dffb3da7f80214ac0603b1ebee3796fefc0c67fa
Gabe! <51248378+cristeigabriel@users.noreply.github.com>
2020-02-11 11:19:27 +0200

    📓 example fixed to latest

--- examples/colors.cpp
+++ examples/colors.cpp
@@ -1,11 +1,12 @@
 int main() {
 
   // we are going to initialize our colors
-  helveta::color::color_t<int> color = {255, 255, 255, 220}; // white, alpha 220
-  helveta::color::color_t<int> _color  = {255, 0, 0};        // red, alpha 255
-  helveta::color::color_t<int> __color = {125, 0, 0}; // dark red, alpha 255
+  helveta::color::color_t color   = {255, 255, 255, 220}; // white, alpha 220
+  helveta::color::color_t _color  = {255, 0, 0};          // red, alpha 255
+  helveta::color::color_t __color = {125, 0, 0};          // dark red, alpha 255
 
-  // we are going to get the brightness of color, _color and __color's brightness
+  // we are going to get the brightness of color, _color and __color's
+  // brightness
   std::cout << "color : " << color.brightness()
             << " _color : " << _color.brightness()
             << " __color : " << __color.brightness() << std::endl;
@@ -22,10 +23,4 @@ int main() {
   // we are going to print the alpha of color, _color and __color
   std::cout << "color : " << color.a << " _color : " << _color.a
             << " __color : " << __color.a << std::endl;
-
-  // we are going to print the alpha of color, _color and __color, but by index
-  std::cout << "color : " << color[3] << " _color : " << _color[3]
-            << " __color : " << __color[3] << std::endl;
-
-  return 0;
 }
